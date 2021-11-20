3582199230d04d7fbceefc9e6040d3e749f3e53c
AllanCapistrano <allan.capistrano3@gmail.com>
2021-11-20 18:18:27 -0300

    Add new attributes and format method.

--- src/models/Travel.java
+++ src/models/Travel.java
@@ -5,12 +5,16 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- *
+ * Trajeto entre a cidade de origem e a cidade de destino.
+ * 
  * @author Allan Capistrano
  * @author João Erick Barbosa
  */
-public class Travel implements Serializable{
+public class Travel implements Serializable {
+
     private LinkedList<List<Edge>> route;
+    private int totalParts;
+    private String cities;
 
     public Travel() {
         this.route = new LinkedList<>();
@@ -18,5 +22,28 @@ public class Travel implements Serializable{
 
     public LinkedList<List<Edge>> getRoute() {
         return route;
+    }
+
+    public void format() {
+        this.totalParts = this.route.size();
+        formatCities();
+    }
+
+    private void formatCities(){
+        this.cities = "";
+        for (int j = 0; j < this.route.size(); j++) {
+            this.cities += this.route.get(j).get(0).getFirstCity().getCityName() + " -> ";
+            if(j == this.route.size() - 1){
+                this.cities += this.route.get(j).get(0).getSecondCity().getCityName();
+            }
+        }
+    }
+
+    public int getTotalParts() {
+        return totalParts;
+    }
+
+    public String getCities() {
+        return cities;
     }
 }
